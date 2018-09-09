f81889fc0f5be2e6a5505bf633eda4121c20c4f3
zlehmann@gmail.com <zlehmann@gmail.com>
2018-09-09 01:55:22 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-# If you shout, she can hear you (or at least she thinks so) 
+# If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
 # NO, NOT SINCE 1938!
@@ -14,7 +14,7 @@
 def speak_to_grandma(string)
   if string == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif string == string.upcase 
+  elsif string == string.upcase
     return "NO, NOT SINCE 1938"
   else
     return "HUH?! SPEAK UP, SONNY!"
